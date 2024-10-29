dab0e02a3fe9ac44fac7e39883ba3e38af24ac8b
Alex Stocco <alexstocco.senai@gmail.com>
2024-10-29 01:22:19 -0300

    teste

---
+++ Utillits/Session.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAgenda.Utillits
+{
+    static class Session
+    {
+        public static bool EstaLogado {  get; set; }
+        public static string Nome { get; set; }
+        public static string Login { get; set; }
+    }
+}
