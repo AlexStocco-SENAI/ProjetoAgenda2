f73b2ba963b6dc5672599fa6bb853d886c120515
André <andre.bido4406@gmail.com>
2024-11-05 09:52:44 -0300

    login

--- Controller/UsuarioController.cs
+++ Controller/UsuarioController.cs
@@ -12,37 +12,82 @@ namespace ProjetoAgenda.Controller
     {
         public bool AddUsuario(string nome, string usuario, string telefone, string senha)
         {
-            //Cria a conexão, estou utilizando a classe ConexaoDB que está dentro da pasta DATA
-            MySqlConnection conexao = ConexaoDB.CriarConexao();
-
-            //Comando SQL que será executado
-            string sql = "INSERT INTO tbUsuarios (nome, usuario, telefone, senha) VALUES (@nome, @usuario, @telefone, @senha);";
-
-            //Abri a conexão com o banco
-            conexao.Open();
-
-            //Esse cara é o responsavel por executar o comando SQL
-            MySqlCommand comando = new MySqlCommand(sql, conexao);
-
-            //Estou trocanco o valor dos @ pelas informações que serão cadastradas
-            //Essas informações vieram dos parametros da função
-            comando.Parameters.AddWithValue("@nome", nome);
-            comando.Parameters.AddWithValue("@usuario", usuario);
-            comando.Parameters.AddWithValue("@telefone", telefone);
-            comando.Parameters.AddWithValue("@senha", senha);
-
-            //Executando no banco de dados
-            int linhasAfetadas = comando.ExecuteNonQuery();
-
-            conexao.Close();
-
-            if (linhasAfetadas > 0)
+            try
             {
-                return true;
+                //Cria a conexão, estou utilizando a classe ConexaoDB que está dentro da pasta DATA
+                MySqlConnection conexao = ConexaoDB.CriarConexao();
+
+                //Comando SQL que será executado
+                string sql = "INSERT INTO tbUsuarios (nome, usuario, telefone, senha) VALUES (@nome, @usuario, @telefone, @senha);";
+
+                //Abri a conexão com o banco
+                conexao.Open();
+
+                //Esse cara é o responsavel por executar o comando SQL
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+                //Estou trocanco o valor dos @ pelas informações que serão cadastradas
+                //Essas informações vieram dos parametros da função
+                comando.Parameters.AddWithValue("@nome", nome);
+                comando.Parameters.AddWithValue("@usuario", usuario);
+                comando.Parameters.AddWithValue("@telefone", telefone);
+                comando.Parameters.AddWithValue("@senha", senha);
+
+                //Executando no banco de dados
+                int linhasAfetadas = comando.ExecuteNonQuery();
+
+                conexao.Close();
+
+                if (linhasAfetadas > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
-            else
+            catch (Exception erro)
+            {
+                MessageBox.Show($"Erro ao efetuar o cadastro: {erro.Message}");
+                return false;
+            }
+        }
+        
+        public bool LogarUsuario(string usuario, string senha)
+        {
+            try
+            {
+                MySqlConnection conexao = ConexaoDB.CriarConexao();
+
+                string sql = @"select * from tbusuarios
+                               where usuario = @usuario
+                               and BINARY senha = @senha;";
+
+                conexao.Open();
+
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+                comando.Parameters.AddWithValue("@usuario", usuario);
+                comando.Parameters.AddWithValue("@senha", senha);
+
+                MySqlDataReader resultado =  comando.ExecuteReader();
+                
+                if (resultado.Read()) {
+                    conexao.Close();
+                    return true;
+                }
+                else
+                {
+                    conexao.Close();
+                    return false;
+                }
+
+            }
+            catch
             {
                 return false;
             }
+        }
     }
 }
---
+++ FrmTeste.cs
@@ -0,0 +1,31 @@
+﻿
+using ProjetoAgenda.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoAgenda
+{
+    public partial class FrmTeste : Form
+    {
+        public FrmTeste()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UsuarioController controleUsuario  = new UsuarioController();
+
+            bool resultado = controleUsuario.LogarUsuario("Godo", "Alexindao");
+
+            MessageBox.Show(resultado.ToString());
+        }
+    }
+}
